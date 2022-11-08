54cc35d6ed5324e44a436ad0806771473c77ace0
Camsmith33 <115576284+Camsmith33@users.noreply.github.com>
2022-11-07 21:03:20 -0600

    Add files via upload

--- object.js
+++ object.js
@@ -1,53 +1,52 @@
-function Info(name, id, shift, payrate){
+function info(){
     let name = document.getElementById("name").value;
-    let id = document.getElementById("id").value;
+    let ident = document.getElementById("ident").value;
     let shift = document.getElementById("shift").value;
     let payrate = document.getElementById("payrate").value;
-    const sara = new ProductionWorker(name, id, shift, payrate)
-    document.getElementById('nameout').innerhtml=sara.name;
-    document.getElementById('idout').innerhtml=sara.id;
+    const sara = new ProductionWorker(name, ident, shift, payrate);
+    console.log(sara.name);
+    console.log(sara.shift);
+    console.log(sara.ident);
+    console.log(sara.payrate);
+    document.getElementById('name').innerhtml=sara.name;
+    document.getElementById('identout').innerhtml=sara.ident;
     document.getElementById('shiftout').innerhtml=sara.shift;
     document.getElementById('payrateout').innerhtml=sara.payrate;
-    alert("Yes")
 }
 class Employee {
-    constructor(name, id){
+    constructor(name, ident){
         this._name = name;
-        this._id = id;
+        this._ident = ident;
     }
-get name(){
-    return this._name;
-}
-set name(newname){
-    this._name = newname;
-}
-get id(){
-    return this._id;
-}
-set id(newid){
-    this._id = newid;
-}
+    get name(){
+        return this._name;
+    }
+    set name(newname){
+        this._name = newname;
+    }
+    get ident(){
+        return this._ident;
+    }
+    set ident(newident){
+        this._ident = newident;
+    }
 }
 class ProductionWorker extends Employee {
-    constructor(name, id, shift, payrate){
-    super(name, id)
+    constructor(name, ident, shift, payrate){
+    super(name, ident)
     this._shift = shift;
     this._payrate = payrate;
     }
-get shift() {
-    return this._shift;
+    get shift() {
+        return this._shift;
+    }
+    set shift(newshift) {
+        this._shift = newshift;
+    }
+    get payrate() {
+        return this._payrate;
+    }
+    set shift(newpayrate) {
+        this._payrate = newpayrate;
+    }
 }
-set shift(newshift) {
-    this._shift = newshift;
-}
-get payrate() {
-    return this._payrate;
-}
-set shift(newpayrate) {
-    this._payrate = newpayrate;
-}
-}
-console.log(sara.name);
-console.log(sara.shift);
-console.log(sara.id);
-console.log(sara.payrate);
